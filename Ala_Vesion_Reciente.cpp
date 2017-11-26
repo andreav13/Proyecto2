95aaaa6253222ae82a1bf794e05334829834c217
Juansebae <31037176+Juansebae@users.noreply.github.com>
2017-11-26 14:42:24 -0500

    Evaluando los resultados de Operaciones Matrices
    Corregí un error que habia en la matriz M version presentación, sin embargo sigue sin funcionar, por alguna razon el rho sigue subiendo hasta infinito. Intente aplicar la forma de inicializar como dijo el profe, tambien intente aplicar la forma de inicializar que dice el articulo, revise cada una de las operaciones entre las matrices corroborando resultados en matlab pero todas dan bien. La verdad nu se que más hacer! Ayudaaaaaa Q.Q! El numero de celdas es Lx=1 y Ly=1 para visualizar mejor y el tiempo esta en 1 para ver las matrices. Ahora despues de las 3 debo salir pero cualquier novedad estaré atento del celular! Perdon..de verdad lo intente.

--- Ala_Vesion_Reciente.cpp
+++ Ala_Vesion_Reciente.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-const int Lx=10;
-const int Ly=6;
+const int Lx=1;
+const int Ly=1;
 const int Q=9;
 const double W0=4/9.;
 
@@ -70,7 +70,7 @@ double LatticeBoltzmann::rho(int ix, int iy, bool UseNew){
   int i; double suma;
   for(suma=0,i=0;i<Q;i++)
     if(UseNew)
-    suma+=fnew[ix][iy][i];
+      suma+=fnew[ix][iy][i];
     else
       suma+=f[ix][iy][i];
   return suma;
@@ -96,13 +96,13 @@ double LatticeBoltzmann::Jy(int ix, int iy, bool UseNew){
   return suma;
 }
 
-/*double LatticeBoltzmann::fequilibrio(int i, double rho0, double Jx0, double Jy0){
+double LatticeBoltzmann::fequilibrio(int i, double rho0, double Jx0, double Jy0){
   double U2, UdotVi;
   U2=(Jx0*Jx0+Jy0*Jy0)/rho0;   UdotVi=(Jx0*V[0][i]+Jy0*V[1][i])/rho0;
   return w[i]*rho0*(1 + 3*UdotVi + 9/2.*UdotVi*UdotVi - 3/2.*U2);
   
 }
-*/
+
 
 double LatticeBoltzmann::zetaequilibrio(int i, double rho0, double Jx0, double Jy0){
   
@@ -128,15 +128,15 @@ void LatticeBoltzmann::Inicie(double rho0, double Jx0, double Jy0){
   int ix,iy,i,j;
   double a=1/9.,b=1/6.,c=1/18.,d=1/36.,e=1/4.,g=1/12.;
 
-  /*int M[Q][Q]={  { 1,  1, 1, 1,  1, 1, 1, 1, 1},   //version presentacion
+  int M[Q][Q]={  { 1,  1, 1, 1,  1, 1, 1, 1, 1},   //version presentacion
 		 { 0,  1, 1, 0, -1,-1,-1, 0, 1},
 		 { 0,  0, 1, 1,  1, 0,-1,-1, -1},
-		 { 0, -2, 1, 0,  1, 2,-1, 0, 1},
+		 { 0, -2, 1, 0, -1, 2,-1, 0, 1},
 		 { 0,  0, 1,-2,  1, 0,-1, 2, -1},
 		 { 4, -2, 1,-2,  1,-2, 1,-2, 1},
 		 {-4, -1, 2,-1,  2,-1, 2,-1, 2},
 		 { 0,  1, 0,-1,  0, 1, 0,-1, 0},
-		 { 0,  0, 1, 0, -1, 0, 1, 0, -1}};*/
+		 { 0,  0, 1, 0, -1, 0, 1, 0, -1}};
 
   /*  int M[Q][Q]={{1,1,1,1,1,1,1,1,1},                  //version articulo
 	       {-4,-1,-1,-1,-1,2,2,2,2},
@@ -148,30 +148,59 @@ void LatticeBoltzmann::Inicie(double rho0, double Jx0, double Jy0){
 	       {0,1,-1,1,-1,0,0,0,0},
 	       {0,0,0,0,0,1,-1,1,-1}};*/
 
-  double M1[Q][Q] = {{a,0,0, 0,0,a, -a,0,0},    //version presentacion
+  /* double M1[Q][Q] = {{a,0,0, 0,0,a, -a,0,0},    //version presentacion
 		     {a,b,0,-b,0,-c,-d,e,0},
 		     {a,b,b, g,g, d, c,0,e},
-		     {a,0,b, 0,-b,c,d,-e,0},
+		     {a,0,b, 0,-b,-c,-d,-e,0},
 		     {a,-b,b,-g,g,d,c,0,-e},
 		     {a,-b,0,b,0,-c,-d,e,0},
 		     {a,-b,-b,-g,-g,d,c,0,e},
 		     {a,0,-b,0,b,-c,-d,-e,0},
 		     {a,b,-b,g,-g,d,c,0,-e}};
+  */
+
+  /*  double M1[Q][Q] = {{a,-a, a, 0, 0, 0, 0, 0, 0},  //version articulo
+		     {a,-d,-c, b,-b, 0, 0, e, 0},
+		     {a,-d,-c, 0, 0, b,-b,-e, 0},
+		     {a,-d,-c,-b, b, 0, 0, e, 0},
+		     {a,-d,-c, 0, 0,-b, b,-e, 0},
+		     {a, c, d, b, g, b, g, 0, e},
+		     {a, c, d,-b,-g, b, g, 0,-e},
+		     {a, c, d,-b,-g,-b,-g, 0, e},
+		     {a, c, d, b, g,-b,-g, 0,-e}};*/
+  
   
   for(ix=0;ix<Lx;ix++)
     {
       for(iy=0;iy<Ly;iy++)
 	{
-	  for(i=0;i<Q;i++){zeta[ix][iy][i]=zetaequilibrio(i,rho0,Jx0,Jy0);}
-	  //	f[ix][iy][i]=fequilibrio(i,rho0,Jx0,Jy0);
+	  for(i=0;i<Q;i++)
+	    {
+	      //	    zeta[ix][iy][i]=zetaequilibrio(i,rho0,Jx0,Jy0);
+	      f[ix][iy][i]=fequilibrio(i,rho0,Jx0,Jy0);
+	    //	    cout<<zeta[ix][iy][i]<<"\t";
+	    }
+	  //	  cout<<endl;
+	  //	  cout<<"----------------------------------------------------------------------------------"<<endl;
       
 	  for(i=0;i<Q;i++)
 	    {
-	      M1porZeta[i]=0;
-	      for(j=0;j<Q;j++){M1porZeta[i]+=M1[i][j]*zeta[ix][iy][j];}
+	      //	      M1porZeta[i]=0;
+	      MporF[i]=0;
+	      for(j=0;j<Q;j++)
+		{
+		  //M1porZeta[i]+=M1[i][j]*zeta[ix][iy][j];
+		  MporF[i]+=M[i][j]*f[ix][iy][j];
+		}
+	      //  cout<<M1porZeta[i]<<"\t";
 	    }
+	  //	  cout<<endl;
       
-	for(i=0;i<Q;i++){f[ix][iy][i]=M1porZeta[i];}
+	for(i=0;i<Q;i++)
+	  {
+	    //f[ix][iy][i]=M1porZeta[i];
+	    zeta[ix][iy][i]=MporF[i];
+	  }
       
       }
     }
@@ -192,18 +221,19 @@ void LatticeBoltzmann::ImponerCampos(int ix, int iy, double & rho0, double & Jx0
 void LatticeBoltzmann::Colisione(int t){ //de f a fnew
   int ix,iy,i,j; double rho0,Jx0,Jy0;
   double a=1/9.,b=1/6.,c=1/18.,d=1/36.,e=1/4.,g=1/12.;
-
+  
   double M1[Q][Q] = {{a,0,0, 0,0,a, -a,0,0},    //version presentacion
 		     {a,b,0,-b,0,-c,-d,e,0},
 		     {a,b,b, g,g, d, c,0,e},
-		     {a,0,b, 0,-b,c,d,-e,0},
+		     {a,0,b, 0,-b,-c,-d,-e,0},
 		     {a,-b,b,-g,g,d,c,0,-e},
 		     {a,-b,0,b,0,-c,-d,e,0},
 		     {a,-b,-b,-g,-g,d,c,0,e},
 		     {a,0,-b,0,b,-c,-d,-e,0},
 		     {a,b,-b,g,-g,d,c,0,-e}};
-
-  /*  double M1[Q][Q] = {{a,-a, a, 0, 0, 0, 0, 0, 0},  //version articulo
+ 
+ 
+  /*double M1[Q][Q] = {{a,-a, a, 0, 0, 0, 0, 0, 0},  //version articulo
 		     {a,-d,-c, b,-b, 0, 0, e, 0},
 		     {a,-d,-c, 0, 0, b,-b,-e, 0},
 		     {a,-d,-c,-b, b, 0, 0, e, 0},
@@ -211,7 +241,8 @@ void LatticeBoltzmann::Colisione(int t){ //de f a fnew
 		     {a, c, d, b, g, b, g, 0, e},
 		     {a, c, d,-b,-g, b, g, 0,-e},
 		     {a, c, d,-b,-g,-b,-g, 0, e},
-		     {a, c, d, b, g,-b,-g, 0,-e}};*/
+		     {a, c, d, b, g,-b,-g, 0,-e}};
+  */
 
   for(ix=0;ix<Lx;ix++)
     for(iy=0;iy<Ly;iy++){ //para cada celda      
@@ -219,9 +250,27 @@ void LatticeBoltzmann::Colisione(int t){ //de f a fnew
       rho0=rho(ix,iy,false); Jx0=Jx(ix,iy,false); Jy0=Jy(ix,iy,false); //calculo campos
       ImponerCampos(ix,iy,rho0,Jx0,Jy0,t);
       
+
+      for(i=0;i<Q;i++){cout<<zeta[ix][iy][i]<<"\t";}
+      cout<<endl;
+      cout<<"-----------------------------------------------------------------------------------------------"<<endl;
+
+      for(i=0;i<Q;i++){cout<<zetaequilibrio(i,rho0,Jx0,Jy0)<<"\t";}
+      cout<<endl;
+      cout<<"-----------------------------------------------------------------------------------------------"<<endl;
+
+      for(i=0;i<Q;i++){cout<<S[i]<<"\t";}
+      cout<<endl;
+      cout<<"-----------------------------------------------------------------------------------------------"<<endl;
+
+      
       for(i=0;i<Q;i++){
-	deltazeta[ix][iy][i]= S[i]*(zeta[ix][iy][i]-zetaequilibrio(i,rho0,Jx0,Jy0));  
+	deltazeta[ix][iy][i]= S[i]*(zeta[ix][iy][i]-zetaequilibrio(i,rho0,Jx0,Jy0));
+	cout<<deltazeta[ix][iy][i]<<"\t";
       }
+      cout<<endl;
+      cout<<"-----------------------------------------------------------------------------------------------"<<endl;
+
 
       for(i=0;i<Q;i++){
 	M1porDeltazeta[i]=0;
@@ -233,7 +282,8 @@ void LatticeBoltzmann::Colisione(int t){ //de f a fnew
 	//fnew[ix][iy][i]=UmUtau*f[ix][iy][i]+Utau*fequilibrio(i,rho0,Jx0,Jy0);
 	fnew[ix][iy][i]=f[ix][iy][i]+M1porDeltazeta[i]; //evoluciono
 	
-    }  
+    }
+
 }
 
 void LatticeBoltzmann::Adveccione(void){ //de fnew a f
@@ -242,14 +292,14 @@ void LatticeBoltzmann::Adveccione(void){ //de fnew a f
   int M[Q][Q]={  { 1,  1, 1, 1,  1, 1, 1, 1, 1},   //version presentacion
 		 { 0,  1, 1, 0, -1,-1,-1, 0, 1},
 		 { 0,  0, 1, 1,  1, 0,-1,-1, -1},
-		 { 0, -2, 1, 0,  1, 2,-1, 0, 1},
+		 { 0, -2, 1, 0, -1, 2,-1, 0, 1},
 		 { 0,  0, 1,-2,  1, 0,-1, 2, -1},
 		 { 4, -2, 1,-2,  1,-2, 1,-2, 1},
 		 {-4, -1, 2,-1,  2,-1, 2,-1, 2},
 		 { 0,  1, 0,-1,  0, 1, 0,-1, 0},
 		 { 0,  0, 1, 0, -1, 0, 1, 0, -1}};
 
-  /*int M[Q][Q]={{1,1,1,1,1,1,1,1,1},                  //version articulo
+  /*  int M[Q][Q]={{1,1,1,1,1,1,1,1,1},                  //version articulo
 	       {-4,-1,-1,-1,-1,2,2,2,2},
 	       {4,-2,-2,-2,-2,1,1,1,1},
 	       {0,1,0,-1,0,1,-1,-1,1},
@@ -257,8 +307,7 @@ void LatticeBoltzmann::Adveccione(void){ //de fnew a f
 	       {0,0,1,0,-1,1,1,-1,-1},
 	       {0,0,-2,0,2,1,1,-1,-1},
 	       {0,1,-1,1,-1,0,0,0,0},
-	       {0,0,0,0,0,1,-1,1,-1}};
-  */
+	       {0,0,0,0,0,1,-1,1,-1}};*/
   
  
   for(ix=0;ix<Lx;ix++)
@@ -267,13 +316,24 @@ void LatticeBoltzmann::Adveccione(void){ //de fnew a f
 	{
 	  
 	  for(i=0;i<Q;i++)
+	    {
 	    f[(ix+V[0][i]+Lx)%Lx][(iy+V[1][i]+Ly)%Ly][i]=fnew[ix][iy][i];
+	    //	    cout<<f[(ix+V[0][i]+Lx)%Lx][(iy+V[1][i]+Ly)%Ly][i]<<"\t";
+	    }
+	  //	  cout<<endl;
+	  //	  cout<<"-----------------------------------------------------------------------------------------------"<<endl;
 	  
-	  for(i=0;i<Q;i++){
+	  
+	  for(i=0;i<Q;i++)
+	    {
 	    MporF[i]=0;
 	    for(j=0;j<Q;j++)
+	      {
 	      MporF[i]+=M[i][j]*f[ix][iy][j];
-	  }
+	      }
+	    //	    cout<<MporF[i]<<"\t";
+	    }
+	  //	  cout<<endl;
       
 	  for(i=0;i<Q;i++)
 	    zeta[(ix+V[0][i]+Lx)%Lx][(iy+V[1][i]+Ly)%Ly][i]=MporF[i];
@@ -317,26 +377,31 @@ int main(void){
   */
 
 
-  
+ 
   //Corra
-  for(t=0;t<tmax;t++){
+  for(t=0;t<1;t++){
+    
+    /*
     for (int ix=0;ix<Lx;ix++)
       {
+	
 	for(int iy=0;iy<Ly;iy++)
 	  {
-	    double Rho0=Ala.rho(ix,iy,false);
-	    double Jx0=Ala.Jx(ix,iy,false);
-	    double Jy0=Ala.Jy(ix,iy,false);
+	    double Rho0=Ala.rho(ix,iy,true);
+	    double Jx0=Ala.Jx(ix,iy,true);
+	    double Jy0=Ala.Jy(ix,iy,true);
 	    Ala.ImponerCampos(ix,iy,Rho0,Jx0,Jy0,t);
 	    cout<<ix<<"\t"<<iy<<"\t"<<Rho0<<"\t"<<Jx0<<"\t"<<Jy0<<endl;
 	  }
       }
-    cout<<"--------------------Nuevo tiempo = "<<t+1<<" ---------------------------------------"<<endl;
+      cout<<"--------------------Nuevo tiempo = "<<t+1<<" ---------------------------------------"<<endl;
+	*/
+    
 
     Ala.Colisione(t);
     Ala.Adveccione();
   }
-  
+
   //  Ala.Imprimase("Ala.dat", t);
   
 
